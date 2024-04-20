ab6961c97d93e209caac6673c69d281ae5661ad0
kristieg <earsy@earsy>
2024-04-20 15:28:50 +0300

    filter

--- filter.py
+++ filter.py
@@ -1,5 +1,4 @@
-#Проверь у себя как работает, у меня быстро, если еще хочешь добавить маты добавляй
-#<3
+import string
 
 bad_words = [ 'бля', 'блядь', 'блять', 'b3ъeб', 'cock', 
              'cunt', 'e6aль', 'ebal', 'eblan', 'eбaл', 
@@ -80,10 +79,40 @@ bad_words = [ 'бля', 'блядь', 'блять', 'b3ъeб', 'cock',
              'хуеныш', 'хуенький', 'хуеплет','хуеплёт', 'хуепромышленник', 'хуерик', 'хуерыло', 'хуесос', 'хуесоска',
              'хуета', 'хуетень', 'хуею', 'хуи', 'хуй', 'хуйком', 'хуйло', 'хуйня', 'хуйрик', 'хуище', 'хуля', 'хую',
              'хуюл', 'хуя', 'хуяк', 'хуякать', 'хуякнуть', 'хуяра', 'хуясе', 'хуячить', 'целка', 'чмо',
-             'чмошник', 'чмырь', 'шалава', 'шалавой', 'шараёбиться', 'шлюха', 'шлюхой', 'шлюшка', 'даун', 'заебись', 'пиdop']
+             'чмошник', 'чмырь', 'шалава', 'шалавой', 'шараёбиться', 'шлюха', 'шлюхой', 'шлюшка', 'даун', 'заебись', 'пиdop', 'член']
+
+#Фраза, которую будем проверять.
+phrase = input("Введите фразу для проверки: ").lower().replace(" ", "")
+
+def distance(a, b): 
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n, m)) space
+        a, b = b, a
+        n, m = m, n
+
+    current_row = range(n + 1)  # Keep current and previous row, not entire matrix
+    for i in range(1, m + 1):
+        previous_row, current_row = current_row, [i] + [0] * n
+        for j in range(1, n + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            if a[j - 1] != b[i - 1]:
+                change += 1
+            current_row[j] = min(add, delete, change)
+
+    return current_row[n]
 
 
-message = input("Введите ваше сообщение: ")
-message = message.lower()
+#Проходимся по всем словам.
+for word in bad_words:
+    #Разбиваем слово на части, и проходимся по ним.
+    for part in range(len(phrase)):
+        #Вот сам наш фрагмент.
+        fragment = phrase[part: part+len(word)]
+        #Если отличие этого фрагмента меньше или равно 25% этого слова, то считаем, что они равны.
+        if distance(fragment, word) <= len(word)*0.25:
+            #Если они равны, выводим надпись о их нахождении.
+            print("False")
 
-print(any(i in bad_words for i in message.split()))
+
